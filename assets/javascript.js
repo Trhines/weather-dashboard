5f112ce36ba615f2f792f9bcfee2fa51faba470b
Trenton Rhines <trhines94@gmail.com>
2021-05-21 22:51:22 -0700

    styling done

--- assets/javascript.js
+++ assets/javascript.js
@@ -40,6 +40,7 @@ function onRefresh(){
     
 }
 
+
 //appnds new search entry to array in local storage
 function updateHistory(search){
     console.log("updating search history")
@@ -47,7 +48,7 @@ function updateHistory(search){
     parsedHistory = JSON.parse(rawHistory)
     parsedHistory.unshift(search)
 
-    if(parsedHistory.length > 10){
+    if(parsedHistory.length > 9){
         parsedHistory.pop()
         console.log("Removed last item from search history")
     }
@@ -56,6 +57,7 @@ function updateHistory(search){
 
     localStorage.setItem('history', JSON.stringify(parsedHistory))
 }
+
 
 //creates search history list, first deletes current list, then repopulates from "history" in local.
 //deleting avoids having items appear in the wrong order
@@ -84,6 +86,18 @@ function populateList(array){
     })
 }
 
+
+//converts unix to readable date
+function getDate(unixTimeStamp){
+    console.log("formatting date")
+    milliseconds = unixTimeStamp * 1000
+    const dateObject = new Date(milliseconds)
+    const month = dateObject.toLocaleString("en-US", {month: "numeric"})
+    const day = dateObject.toLocaleString("en-US", {day: "numeric"})
+    const year = dateObject.toLocaleString("en-US", {year: "numeric"})
+    return month+"/"+day+"/"+year
+}
+
 function callApi(search){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=ffbfb6b231b2c20b9f38afc1cec77543`)
         .then(function (response) {
@@ -96,13 +110,15 @@ function callApi(search){
             }
         })
         .then(function (data){
+            console.log("lat "+data.coord.lat)
+            console.log("lon "+data.coord.lon)
+            oneCallApi(data.coord.lat, data.coord.lon)
             document.getElementById("name").textContent = data.name
-            document.getElementById("temp").textContent = "Tepmerature: " + data.main.temp + "\u00B0F"
-            document.getElementById("humidity").textContent = "Humidity: " + data.main.humidity + "%"
-            document.getElementById("wind").textContent = "Wind Speed: " + data.wind.speed + " mph"   
         })
+}
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&units=imperial&appid=ffbfb6b231b2c20b9f38afc1cec77543`)
+function oneCallApi(lat, lon){
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alert&appid=ffbfb6b231b2c20b9f38afc1cec77543`)
         .then(function (response) {
             if (response.status !== 200) {
                 alert("error." + response.status);
@@ -111,35 +127,80 @@ function callApi(search){
             else {
                 return response.json()
             }
+            return response.json()
         })
         .then(function (data){
-            //running a for loop through this data melted my laptop, but hard coding didnt. ¯\_(ツ)_/¯
-            console.log("second API")
-            //`http://openweathermap.org/img/w/${data.list[2].weather[0].icon}.png`
-            console.log(data.list[2].dt_txt)
-            console.log(data.list[2].main.temp)
-            console.log(data.list[2].main.humidity)
+            let uvi = data.current.uvi
+            //let uvi = 9
+            console.log("secondAPI")
+            console.log(data)
 
-            console.log(data.list[8].dt_txt)
-            console.log(data.list[8].main.temp)
-            console.log(data.list[8].main.humidity)
+            //populates the jumbotron
+            document.getElementById("current-date").textContent = " " + getDate(data.current.dt)
+            document.getElementById("temp").textContent = "Tepmerature: " + data.current.temp + "\u00B0F"
+            document.getElementById("humidity").textContent = "Humidity: " + data.current.humidity + "%"
+            document.getElementById("wind").textContent = "Wind Speed: " + data.current.wind_speed + " mph" 
+            document.getElementById("uv").textContent = "UV: "
+            document.getElementById("uvi").textContent = uvi
+            document.getElementById("current-img").src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`
+                if(uvi<2){
+                  document.getElementById("uvi").classList.add('green')
+                  document.getElementById("uvi").classList.remove('yellow')
+                  document.getElementById("uvi").classList.remove('red')
+                }
+                else if(uvi > 2 && uvi < 7){
+                  document.getElementById("uvi").classList.add('yellow')
+                  document.getElementById("uvi").classList.remove('green')
+                  document.getElementById("uvi").classList.remove('red')
+                }
+                else if(uvi>7){
+                  document.getElementById("uvi").classList.add('red')
+                  document.getElementById("uvi").classList.remove('yellow')
+                  document.getElementById("uvi").classList.remove('green')
+                }
+    
+            
+            //populates cards
 
-            console.log(data.list[16].dt_txt)
-            console.log(data.list[16].main.temp)
-            console.log(data.list[16].main.humidity)
+            document.getElementById("date-1").textContent = getDate(data.daily[1].dt)
+            document.getElementById("img-1").src = `http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}.png`
+            document.getElementById("temp-1").textContent = "Tepmerature " + data.daily[1].temp.day + "\u00B0F"
+            document.getElementById("hu-1").textContent = "Humidity " + data.daily[1].humidity + "%"
 
-            console.log(data.list[2].dt_txt)
-            console.log(data.list[2].main.temp)
-            console.log(data.list[2].main.humidity)
+            document.getElementById("date-2").textContent = getDate(data.daily[2].dt)
+            document.getElementById("img-2").src = `http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}.png`
+            document.getElementById("temp-2").textContent = "Tepmerature: " + data.daily[2].temp.day + "\u00B0F"
+            document.getElementById("hu-2").textContent = "Humidity: " + data.daily[2].humidity + "%"
 
-            console.log(data.list[2].dt_txt)
-            console.log(data.list[2].main.temp)
-            console.log(data.list[2].main.humidity)    
-        })
+            document.getElementById("date-3").textContent = getDate(data.daily[3].dt)
+            document.getElementById("img-3").src = `http://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}.png`
+            document.getElementById("temp-3").textContent = "Tepmerature " + data.daily[3].temp.day + "\u00B0F"
+            document.getElementById("hu-3").textContent = "Humidity " + data.daily[3].humidity + "%"
+
+            document.getElementById("date-4").textContent = getDate(data.daily[4].dt)
+            document.getElementById("img-4").src = `http://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}.png`
+            document.getElementById("temp-4").textContent = "Tepmerature " + data.daily[4].temp.day + "\u00B0F"
+            document.getElementById("hu-4").textContent = "Humidity " + data.daily[4].humidity + "%"
+
+            document.getElementById("date-5").textContent = getDate(data.daily[5].dt)
+            document.getElementById("img-5").src = `http://openweathermap.org/img/wn/${data.daily[5].weather[0].icon}.png`
+            document.getElementById("temp-5").textContent = "Tepmerature " + data.daily[5].temp.day + "\u00B0F"
+            document.getElementById("hu-5").textContent = "Humidity " + data.daily[5].humidity + "%"
+
+
+    })
+
 }
 
-function dipslayInfo(){
+    
 
-}
+
+
 
 onRefresh()
+
+
+
+
+
+
